01313db7d34d7991d117de7586b6054468ac1a9d
susu032191 <373841272@qq.com>
2024-06-12 11:21:01 +0800

    vuex复习

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,65 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <!-- <p>{{ welcomeMessage }}</p>
+    <p>年龄：{{ age }}</p>
+    <button @click="updateUsername">更改用户名为 '李四'</button>
+    <button @click="getUser">异步获取新用户名</button> -->
+    <div>
+      <h1>我是moduleA模块</h1>
+      <p>{{ welcomeMessageA }}</p>
+      <!-- <p>{{ $store.getters.welcomeMessage }}</p> -->
+      <P>年龄：{{ ageA }}</P>
+    </div>
+    <div>
+      <h1>我是moduleB模块</h1>
+      <p>{{ welcomeMessageB }}</p>
+      <p>年龄：{{ ageB }}</p>
+      <button @click="updateUsername">更改用户名为 'moduleB-susu'</button>
+      <button @click="getUser">异步获取新用户名</button>
+    </div>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { useStore } from 'vuex'
+import { computed } from 'vue'
 
-nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  setup () {
+    // 根组件vuex的使用方法
+    const store = useStore()
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    // // 计算属性来获取 Vuex state
+    // const welcomeMessage = computed(() => store.getters.welcomeMessage)
+    // const age = computed(() => store.state.age)
 
-    &.router-link-exact-active {
-      color: #42b983;
+    // // 方法来提交 mutation
+    // const updateUsername = () => {
+    //   store.commit('setUsername', '李四')
+    // }
+
+    // // 方法来派发 action
+    // const getUser = () => {
+    //   store.dispatch('fetchUser')
+    // }
+
+    // return { welcomeMessage, age, updateUsername, getUser }
+    const welcomeMessageA = computed(() => store.getters.welcomeMessage)
+    const ageA = computed(() => store.state.moduleA.age)
+
+    const welcomeMessageB = computed(() => store.getters['moduleB/welcomeMessage'])
+    const ageB = computed(() => store.state.moduleB.age)
+
+    const updateUsername = () => {
+      store.commit('moduleB/setUsername', 'moduleB-susu')
     }
+
+    const getUser = () => {
+      store.dispatch('moduleB/fetchUser')
+    }
+
+    return { welcomeMessageA, ageA, welcomeMessageB, ageB, updateUsername, getUser }
   }
 }
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,83 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+// A模块
+const moduleA = {
+  // namespaced: true,
   state: {
+    username: 'moduleA',
+    age: 20
   },
   getters: {
+    welcomeMessage: (state) => {
+      return `欢迎，${state.username}！`
+    }
+  }
+}
+
+// B模块
+const moduleB = {
+  namespaced: true,
+  state: {
+    username: 'moduleB',
+    age: 25
+  },
+  getters: {
+    welcomeMessage: (state) => {
+      return `欢迎，${state.username}！`
+    }
   },
   mutations: {
+    setUsername (state, newUsername) {
+      state.username = newUsername
+    },
+    incrementAge (state) {
+      state.age += 1
+    }
   },
   actions: {
-  },
+    fetchUser: (ctx) => {
+      setTimeout(() => {
+        ctx.commit('setUsername', 'moduleB-yangsusu')
+      }, 1000)
+    }
+  }
+}
+
+export default createStore({
   modules: {
+    moduleA,
+    moduleB
   }
 })
+
+// 根模块vuex使用
+// export default createStore({
+//   state: {
+//     username: '张三',
+//     age: 20
+//   },
+//   getters: {
+//     // 加上欢迎词
+//     welcomeMessage: (state) => {
+//       return `欢迎，${state.username}！`
+//     }
+//   },
+//   mutations: {
+//     // 修改用户名
+//     setUsername (state, newUsername) {
+//       state.username = newUsername
+//     },
+//     // 增加年龄
+//     incrementAge (state) {
+//       state.age += 1
+//     }
+//   },
+//   actions: {
+//     // 异步获取用户信息
+//     fetchUser (ctx) {
+//       setTimeout(() => {
+//         ctx.commit('setUsername', '李四')
+//       }, 1000)
+//     }
+//   }
+// })
